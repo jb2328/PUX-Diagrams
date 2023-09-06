2a281e4a10caad5e15486c93baa737ba548df3e3
Justas Brazauskas <jb2328@cam.ac.uk>
2023-09-06 16:56:52 +0100

    arrows, new heights and full dataset

--- viz_script.js
+++ viz_script.js
@@ -80,28 +80,7 @@ const secondaryData=[
     {'name': 'VE4', 'in': ['IE6'], 'out': ['SE1']},
     {'name': 'SE1', 'in': ['TE2'], 'out': ['ME2']},
     {'name': 'TE2', 'in': ['ME4'], 'out': ['ME6', 'CE4']},
-    // {'name': 'PE4','in': ['CE3', 'ME5', 'PE6'],'out': ['IE6',  'TE4']},
-    // {'name': 'VE3', 'in': ['SE2', 'PE1', 'IE1', 'CE2', 'PE6'], 'out': ['ME4', 'TE5', 'VE1', 'VE4']},
-    // {'name': 'ME6', 'in': ['VE4'], 'out': ['CE3', 'ME4', 'IE5']},
-    // {'name': 'SE2','in': ['ME2', 'IE2', 'CE1', 'PE2'],'out': ['TE2', 'TE3', 'TE5', 'PE5', 'SE3', 'ME3']},
-    // {'name': 'CE3','in': ['PE5', 'IE1'],'out': [ 'TE2', 'ME6', 'ME4']},
-    // {'name': 'ME1','in': ['IE2', 'ME3'],'out': ['ME6', 'PE1', 'TE4', 'VE3', 'IE1']},
-    // {'name': 'CE4','in': ['ME3', 'PE1'],'out': ['SE2', 'SE3', 'PE4', 'CE3', 'IE4', 'VE5']},
-    // {'name': 'CE2', 'in': ['IE5', 'SE3', 'CE4', 'VE3'], 'out': ['ME1']},
-    // {'name': 'IE5', 'in': ['ME1'], 'out': ['IE6', 'CE4', 'ME2', 'IE3', 'PE3']},
-    // {'name': 'TE5', 'in': ['IE4'], 'out': ['TE4']},
-    // {'name': 'IE4','in': ['CE2', 'CE4', 'CE3', 'IE3', 'PE3', 'ME2'], 'out': ['SE1', 'TE4', 'TE5', 'PE2', 'PE6']},
-    // {'name': 'TE1', 'in': ['SE4', 'VE3', 'SE3', 'VE5'], 'out': ['TE2']},
-    // {'name': 'PE5','in': ['ME6', 'PE6', ],'out': ['ME4', 'IE2', 'TE4', 'ME1', 'IE4']},
-    // {'name': 'SE4','in': ['IE2', 'IE1', 'ME6'],'out': ['SE1', 'TE5', 'PE6', 'IE5', 'SE3', 'ME1']},
-    // {'name': 'PE1', 'in': ['PE5'], 'out': ['SE2', 'CE2', 'VE4', 'CE4', 'IE4']},
-    // {'name': 'PE2', 'in': ['PE6'], 'out': ['ME2', 'VE5', 'SE2', 'IE1']},
-    // {'name': 'ME3', 'in': ['ME4', 'IE4', 'PE1'], 'out': ['ME6', 'PE6', 'PE4']},
-    // {'name': 'VE1', 'in': ['PE5'], 'out': ['PE6', 'ME2', 'IE3', 'VE4']},
-    // {'name': 'ME5', 'in': ['ME6'], 'out': ['PE3', 'IE1']},
-    // {'name': 'TE3','in': ['PE1', 'VE2', 'TE5', 'IE3', 'PE2', 'VE5'],'out': ['TE1', 'SE2']},
-    // {'name': 'IE6','in': ['ME5', 'ME1', 'IE4', 'CE1', 'SE1'],'out': ['PE6', 'CE3', 'PE1', 'TE4', 'ME2', 'ME3']},
-    // {'name': 'TE4', 'in': ['IE2', 'IE3'], 'out': ['CE1', 'SE1']}
+
 ];
 
 
@@ -120,6 +99,24 @@ const secondaryData=[
     .attr("width", width)
     .attr("height", height);
   
+
+    svg.append("defs").append("marker")
+    .attr("id", "arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 5)
+    .attr("refY", 0)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M0,-5L10,0L0,5")
+    .attr("fill", "none")  // No fill to make it transparent
+    .attr("stroke", "black")  // Outline color
+    .attr("opacity", 0.3)  // Outline color
+    .attr("stroke-width", "1");  // Outline width
+
+
+    
   // Scale for positioning children and parents
   const xScale = d3.scalePoint().domain(uniqueChildren).range([50, width - 50]).padding(0.5);
   const yScale = d3.scalePoint().domain(activitiesWithChildren.map(d => d.name)).range([50, width - 50]).padding(0.5);
@@ -137,55 +134,47 @@ const secondaryData=[
       ];
       svg.append("path")
         .attr("d", line(points))
+        .attr("class", "activities_path")
         .attr("fill", "none")
         .attr("stroke", "#555")
         .attr("stroke-opacity", 0.4);
     });
   });
   
-  // Draw nodes for activities (parents)
-  svg.append("g")
-    .selectAll("text.activities")
-    .data(activitiesWithChildren)
-    .enter().append("text")
-    .attr("x", d => yScale(d.name))
-    .attr("y", 400)
-    .attr("text-anchor", "middle")
-    .text(d => d.name);
-  
-  // Draw nodes for unique children
-  svg.append("g")
-    .selectAll("text.children")
-    .data(uniqueChildren)
-    .enter().append("text")
-    .attr("x", d => xScale(d))
-    .attr("y", 250)
-    .attr("text-anchor", "middle")
-    .text(d => d);
-  
-  // Create links between children based on the "in" and "out" connections
   const childLinks = [];
-  secondaryData.forEach(d => {
-    d.in.forEach(targetName => {
-      childLinks.push({ source: xScale(d.name), target: xScale(targetName), color: "red" });
+  newData.forEach(d => {
+    d.link_positive.forEach(linkObj => {
+      const targetName = Object.keys(linkObj)[0];
+      const strength = linkObj[targetName];
+      childLinks.push({ source_id:d.name,target_id:targetName,source: xScale(d.name), target: xScale(targetName), color: "red", strength: strength });
     });
-    d.out.forEach(targetName => {
-      childLinks.push({ source: xScale(d.name), target: xScale(targetName), color: "blue" });
+    d.link_negative.forEach(linkObj => {
+      const targetName = Object.keys(linkObj)[0];
+      const strength = linkObj[targetName];
+      childLinks.push({ source_id:d.name,target_id:targetName,source: xScale(d.name), target: xScale(targetName), color: "blue", strength: strength });
     });
   });
-  
 
 // Draw arc-shaped links
 svg.append("g")
   .selectAll("path")
   .data(childLinks)
   .enter().append("path")
+  .attr("marker-end", "url(#arrow)")
+  .attr("class", "experiences_path")
+  .attr("id", d=>d.source_id+"-"+d.target_id)
   .attr("d", d => {
     const y = 241; // Y-coordinate of the children
     const midX = (d.source + d.target) / 2;
     const path = d3.path();
     path.moveTo(d.source, y);
-    path.quadraticCurveTo(midX, y - 300+Math.ceil(Math.random()*100), d.target, y); // 100 controls the curvature
+    // path.quadraticCurveTo(midX, y - 300+Math.ceil(Math.random()*100), d.target, y); // 100 controls the curvature
+    // path.quadraticCurveTo(midX, y - (300 * Math.abs(d.strength)), d.target, y);
+    // path.quadraticCurveTo(midX, y - (300 * Math.pow(Math.abs(d.strength), 2)), d.target, y);
+    path.quadraticCurveTo(midX, y - (1150 * Math.pow(Math.abs(d.strength) - 0.5, 1)), d.target, y);
+
+    // path.quadraticCurveTo(midX, y - (300 * Math.pow((d.strength - 0.5) / 0.4, 3)), d.target, y);
+
     return path.toString();
   })
   .attr("fill", "none")
@@ -193,3 +182,72 @@ svg.append("g")
   .attr("stroke-width", d => 1) // Use 'stroke' attribute from the data
   .attr("color", d => d.color) // Use 'stroke' attribute from the data
   .attr("stroke-opacity", 0.4);
+  
+  // Draw nodes for activities (parents)
+  // LOWER NODES
+  svg.append("g")
+    .selectAll("text.activities")
+    .data(activitiesWithChildren)
+    .enter().append("text")
+    .attr("class", "activities_txt")
+    .attr("x", d => yScale(d.name))
+    .attr("y", 410)
+    .attr("text-anchor", "middle")
+    .text(d => d.name)
+    .attr("id", d => "activity-" + d.name);
+  
+  // Draw nodes for unique children
+  // UPPER NODES
+  svg.append("g")
+    .selectAll("text.children")
+    .data(uniqueChildren)
+    .enter().append("text")
+    .attr("class", "experiences_txt")
+    .attr("x", d => xScale(d))
+    .attr("y", 250)
+    .attr("text-anchor", "middle")
+    .text(d => d)
+    .attr("id", d =>"experience-"+d);
+
+    d3.selectAll(".experiences_path")
+    .on("mouseover", function(event, d) {
+
+      console.log(event, d);
+      let source_id=d.source_id;
+      let target_id=d.target_id;
+
+     
+      d3.select("#experience-"+target_id)
+      .style("font-weight", "bolder");
+  
+      d3.select("#experience-"+source_id)
+        .style("font-weight", "bolder");
+
+        svg.selectAll(".experiences_path")
+          .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.1);
+
+
+        d3.select("#"+source_id+"-"+target_id)
+        .attr("stroke", d.color)
+        .attr("stroke-width", 5)
+        .attr("fill", "none")
+        .attr("stroke-opacity", 1);
+      
+        // .style("font-weight", function(p) {
+        //   return p.source === xScale(d) || p.target === xScale(d) ? 1 : 0.4;
+        // });
+    })
+    .on("mouseout", function(event, d) {
+      svg.selectAll(".experiences_txt")
+        .style("font-weight", "normal");
+  
+      svg.selectAll(".experiences_path")
+      .data(childLinks)
+      .attr("fill", "none")
+      .attr("stroke", d => d.color) // Use 'stroke' attribute from the data
+      .attr("stroke-width", d => 1) // Use 'stroke' attribute from the data
+      .attr("color", d => d.color) // Use 'stroke' attribute from the data
+      .attr("stroke-opacity", 0.4);
+      
+    });
