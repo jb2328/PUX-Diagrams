63fd299e77574399e8409a60b62bfc93a778db67
Justas Brazauskas <jb2328@cam.ac.uk>
2023-10-31 14:26:12 +0000

    interaction  cleanup, zoom broken

--- helpers.js
+++ helpers.js
@@ -147,5 +147,266 @@ function getPathString(d) {
     if (id) textElement.attr("id", id);
   }
 
+//------------------------------------------//
+//------------BOTTOM LEFT BULLETS-----------//
+//-----------------------------------------//
 
- 
+//bottom left ACTIVITIES
+  function activity_bullets(d){
+
+    // write the ACTIVITY name and id
+    d3.select("#activity_txt").text(`(${d.name}) ${d.id}`);
+
+     // draw ACTIVITY temp-circles for the list
+     svg
+     .append("g")
+     .attr("class", "temp-circle")
+     .append("circle")
+     .attr("cx", 116.5)
+     .attr("cy", 497)
+     .attr("r", "5px")
+     .style("fill", colorMap[d.name.slice(0, 2)]);
+  }
+
+  //bottom left EXPERIENCES
+ function experience_bullets(d){
+
+    const source = d.name;
+    const targets = d.imports;
+
+    let experiences = "";
+
+    // draw paths from ACTIVITY to EXPERIENCE
+    targets.forEach((target) => {
+      const foundObject = newData.find((item) => item.name === target);
+      experiences += `(${target}) ${foundObject.id}\n`;
+      d3.select(`#${source}-${target}`)
+        .style("stroke", colorMap[source.slice(0, 2)])
+        .style("stroke-width", STROKE_WIDTH_ON)
+        .style("fill", "none")
+        .style("stroke-opacity", OPACITY_ON);
+    });
+
+    const split_text = experiences.split("\n");
+    split_text.pop(); // Remove the last empty string
+
+    // draw EXPERIENCE temp-circles for the list
+    split_text.forEach((text, i) => {
+      svg
+        .append("g")
+        .attr("class", "temp-circle")
+        .append("circle")
+        .attr("cx", 116.5)
+        .attr("cy", 547 + i * 15)
+        .attr("r", "5px")
+        .style("fill", colorMap[text.slice(1, 3)]);
+    });
+
+    // write down entries in the EXPERIENCE list
+    d3.select("#experience_txt")
+      .attr("text-anchor", "start")
+      .selectAll("tspan")
+      .remove()
+      .data(split_text)
+      .enter()
+      .append("tspan")
+      .attr("x", 125)
+      .attr("dy", (d, i) => (i === 0 ? 0 : 15))
+      .text((d) => d);
+}
+
+//both activites and experiences (bottom left)
+function combined_bullets(d,parent_list){
+    const foundObject = newData.find((item) => item.name === d);
+    const newExperience = `(${d})  ${foundObject.id} \n`;
+    const split_experiences = newExperience.split("\n");
+
+    let split_activities = parent_list.split("\n");
+
+    updateText("#activity_txt", split_activities,125);
+    updateText("#experience_txt", split_experiences,125);
+    appendCircles(svg, split_activities, 497, colorMap);
+    appendCircles(svg, split_experiences, 547, colorMap);
+
+    d3.select("#experience_txt")
+      .text(" (" + d + ")  " + foundObject.id + " \n");
+
+}
+//------------------------------------------//
+//----------BOTTOM LEFT BULLETS END---------//
+//------------------------------------------//
+
+function set_html_text(d, mode='experience'){
+
+    //account for different data formatting for activities
+    d=mode=='activity'?d.name:d;
+
+    document.getElementById("pux_header_id").innerHTML=pux_list_definitions[d.slice(0, 2)]+" "+ PUX_COMPLETE[d].id[2];
+    document.getElementById("pux_header_id").style.color = colorMap[d.slice(0, 2)];
+
+    document.getElementById("pux_header_name").innerHTML=": "+PUX_COMPLETE[d].name;
+
+    document.getElementById("short_paragraph").innerHTML=PUX_COMPLETE[d].short_description;
+    document.getElementById("long_paragraph").innerHTML=PUX_COMPLETE[d].long_description;
+
+
+}
+
+function clear_html_text(){
+    document.getElementById("pux_header_id").innerHTML="";
+    document.getElementById("pux_header_name").innerHTML="";
+
+    document.getElementById("short_paragraph").innerHTML="";
+    document.getElementById("long_paragraph").innerHTML="";
+}
+
+// clear d3 text and icons after mouseout on ACTIVITY circles
+function clear_bullets(){
+    d3.selectAll(".temp-circle").remove();
+    d3.select("#activity_txt").text("");
+    d3.select("#experience_txt").text("");
+
+    d3.selectAll(".activities_path")
+      .style("fill", "none")
+      .style("stroke", STROKE_COLOR_OFF)
+      .style("stroke-width", STROKE_WIDTH_OFF)
+      .style("stroke-opacity", OPACITY_OFF);
+
+      d3.select("#positive_experience").text("");
+      d3.select("#negative_experience").text("");
+}
+
+function find_experience_parents(d){
+
+    let parent_text = "";
+    let parents_all = findNameByImport(d);
+
+    // console.log("PARENT LIST", parents_all,"FROM",d)
+    parents_all.forEach((entry) => {
+      const found_parent = inputData.find((item) => item.name === entry);
+    //   console.log("FOUND PARENT", found_parent)
+      parent_text += `(${entry})  ${found_parent.id} \n`;
+      d3.select(`#${entry}-${d}`)
+        .style("stroke", colorMap[entry.slice(0, 2)])
+        .style("stroke-width", STROKE_WIDTH_ON)
+        .style("stroke-opacity", OPACITY_ON);
+    });
+
+    return parent_text
+}
+
+
+function experience_sentiments_bullets(d){
+
+    // Initialize two arrays to store IDs
+    const firstHalfIDs = [];
+    const secondHalfIDs = [];
+
+    // Select all elements with the class "experiences_path"
+    d3.selectAll(".experiences_path").each(function () {
+      const idParts = this.id.split("-");
+      if (idParts[0].includes(d)) {
+        firstHalfIDs.push(this.id);
+      } else if (idParts[1].includes(d)) {
+        secondHalfIDs.push(this.id);
+      }
+    });
+
+    let positive_experience = "";
+    let negative_experience = "";
+
+    // Loop through and target elements with specificString in the first half
+    firstHalfIDs.forEach((id) => {
+      const target_circle = id.split("-")[1];
+      const element = d3.select("#" + id);
+      const pathLength = element.node().getTotalLength();
+
+      d3.select("#experiences_circle-" + target_circle)
+        .style("opacity",OPACITY_ON);
+
+      element
+        .attr("stroke-dasharray", `${pathLength} ${pathLength}`)
+        .attr("stroke-dashoffset", pathLength)
+        .style("stroke", FORWARD_LINK_COLOR)
+        .style("stroke-width", STROKE_WIDTH_ON)
+        .style("opacity", OPACITY_ON)
+        .transition()
+        .duration(1500)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0);
+
+      let foundObject =
+        newData.find((item) => item.name === target_circle) ||
+        inputData.find((item) => item.name === target_circle);
+
+      if (!foundObject)
+        console.error(`id: ${id}, target: ${target_circle} not found`);
+
+      positive_experience += ` (${target_circle})   ${foundObject.id} \n`;
+    });
+
+    // Loop through and target elements with specificString in the second half
+    secondHalfIDs.forEach((id) => {
+      const target_circle = id.split("-")[0];
+      const element = d3.select("#" + id);
+      const pathLength = element.node().getTotalLength();
+
+      let foundObject =
+        newData.find((item) => item.name === target_circle) ||
+        inputData.find((item) => item.name === target_circle);
+
+      if (!foundObject)
+        console.error(`id: ${id}, target: ${target_circle} not found`);
+
+      negative_experience += ` (${target_circle}) \t ${foundObject.id} \n`;
+
+      d3.select("#experiences_circle-" + target_circle)
+        .style("opacity",OPACITY_ON);
+
+      element
+        .attr("stroke-dasharray", `${pathLength} ${pathLength}`)
+        .attr("stroke-dashoffset", pathLength)
+        .style("stroke", BACKWARD_LINK_COLOR)
+        .style("stroke-width", STROKE_WIDTH_ON)
+        .style("opacity", OPACITY_ON)
+        .transition()
+        .duration(1500)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0);
+    });
+
+
+    let split_positive = positive_experience.split("\n");
+    let split_negative = negative_experience.split("\n");
+
+    d3.select("#negative_experience").text("");
+    d3.select("#positive_experience").text("");
+
+    updateTextAndCircles("negative_experience",split_negative,START_NEGATIVE_X,START_Y);
+    updateTextAndCircles("positive_experience",split_positive,START_POSITIVE_X, START_Y);
+}
+
+function clean_experience_paths(){
+    d3.selectAll(".experiences_path")
+    .style("stroke", STROKE_COLOR_OFF)
+    .style("opacity", OPACITY_OFF)
+    .style("stroke-width", STROKE_WIDTH_OFF);
+
+    d3.selectAll(".experiences_path")
+      .style("stroke", STROKE_COLOR_OFF)
+      .style("stroke-width", STROKE_WIDTH_OFF)
+      .style("stroke-opacity", OPACITY_OFF);
+
+    d3.selectAll(".experience_circle")
+      .style("stroke-opacity", OPACITY_ON)
+      .style("opacity", OPACITY_ON);
+
+}
+
+function clean_activities_paths(){
+    d3.selectAll(".activities_path")
+    .style("stroke", STROKE_COLOR_OFF)
+    .style("stroke-width", STROKE_WIDTH_OFF)
+    .style("stroke-opacity", OPACITY_OFF);
+
+}
--- viz_script.js
+++ viz_script.js
@@ -19,10 +19,13 @@ const BACKWARD_LINK_COLOR = "red";
 const OPACITY_ON = 1;
 const OPACITY_OFF = 0.2;
 
+// x pos for negative and positive experiences
 const START_POSITIVE_X = 380;
 const START_NEGATIVE_X = 700;
+// Y pos for negative and positive experiences
+const START_Y = 511.5;
 
-
+// Y pos for CIRCLE rows
 const Y_EXPERIENCES=400;
 const Y_ACTIVITIES=250;
 
@@ -44,12 +47,11 @@ const VIZ_MODE=0;
 // location of the experience id locs
 const EXP_ID_TXT=270;
 
-// Create the SVG container
-// const svg = d3
-//   .select("body")
-//   .append("svg")
-//   .attr("width", width)
-//   .attr("height", height);
+const yChild = Y_ACTIVITIES-9;
+const yParent = Y_EXPERIENCES+8;
+const strokeWidth = 1;
+
+
 const svg = d3
   .select("#svg-container")
   .append("svg")
@@ -109,10 +111,6 @@ inputData.forEach((activity) => {
       .attr("stroke-opacity", OPACITY_OFF);
   });
 });
-
-const yChild = Y_ACTIVITIES-9;
-const yParent = Y_EXPERIENCES+8;
-const strokeWidth = 1;
 
 
 const childLinks = [];
@@ -193,93 +191,7 @@ const nodes = svg
   .attr("r", CIRCLE_RADIUS_PX)
   .attr("fill", (d) => colorMap[d.name.slice(0, 2)]);
 
-
-// ================================================= //
-// =========== ACTIVITY CIRCLES (BOTTOM) =========== //
-// ================================================= //
-
-d3.selectAll(".activity_circle")
-  .on("mouseover", function (event, d) {
-
-    document.getElementById("pux_header_id").innerHTML=pux_list_definitions[d.name.slice(0, 2)]+" "+ PUX_COMPLETE[d.name].id[2];
-    document.getElementById("pux_header_id").style.color = colorMap[d.name.slice(0, 2)];
-
-    document.getElementById("pux_header_name").innerHTML=": "+PUX_COMPLETE[d.name].name;
-    
-    document.getElementById("short_paragraph").innerHTML=PUX_COMPLETE[d.name].short_description;
-    document.getElementById("long_paragraph").innerHTML=PUX_COMPLETE[d.name].long_description;
-
-
-    d3.select("#activity_txt").text(`(${d.name}) ${d.id}`);
-
-    svg
-      .append("g")
-      .attr("class", "temp-circle")
-      .append("circle")
-      .attr("cx", 116.5)
-      .attr("cy", 497)
-      .attr("r", "5px")
-      .style("fill", colorMap[d.name.slice(0, 2)]);
-
-    const source = d.name;
-    const targets = d.imports;
-    let experiences = "";
-
-    targets.forEach((target) => {
-      const foundObject = newData.find((item) => item.name === target);
-      experiences += `(${target}) ${foundObject.id}\n`;
-      d3.select(`#${source}-${target}`)
-        .style("stroke", colorMap[source.slice(0, 2)])
-        .style("stroke-width", STROKE_WIDTH_ON)
-        .style("fill", "none")
-        .style("stroke-opacity", OPACITY_ON);
-    });
-
-    const split_text = experiences.split("\n");
-    split_text.pop(); // Remove the last empty string
-
-    split_text.forEach((text, i) => {
-      svg
-        .append("g")
-        .attr("class", "temp-circle")
-        .append("circle")
-        .attr("cx", 116.5)
-        .attr("cy", 547 + i * 15)
-        .attr("r", "5px")
-        .style("fill", colorMap[text.slice(1, 3)]);
-    });
-
-    d3.select("#experience_txt")
-      .attr("text-anchor", "start")
-      .selectAll("tspan")
-      .remove()
-      .data(split_text)
-      .enter()
-      .append("tspan")
-      .attr("x", 125)
-      .attr("dy", (d, i) => (i === 0 ? 0 : 15))
-      .text((d) => d);
-  })
-  .on("mouseout", function (event, d) {
-    document.getElementById("pux_header_id").innerHTML="";
-    document.getElementById("pux_header_name").innerHTML="";
-
-    document.getElementById("short_paragraph").innerHTML="";
-    document.getElementById("long_paragraph").innerHTML="";
-
-
-    d3.selectAll(".temp-circle").remove();
-    d3.select("#activity_txt").text("");
-    d3.select("#experience_txt").text("");
-
-    d3.selectAll(".activities_path")
-      .style("fill", "none")
-      .style("stroke", STROKE_COLOR_OFF)
-      .style("stroke-width", STROKE_WIDTH_OFF)
-      .style("stroke-opacity", OPACITY_OFF);
-  });
-
-// Draw text labels for activities
+  // Draw text labels for activities
 svg
   .append("g")
   .selectAll("text.activities")
@@ -293,6 +205,32 @@ svg
   .attr("text-anchor", "middle")
   .text((d) => d.name)
   .attr("id", (d) => `activity-${d.name}`);
+
+
+// ================================================= //
+// =========== ACTIVITY CIRCLES (BOTTOM) =========== //
+// ================================================= //
+
+d3.selectAll(".activity_circle")
+  .on("mouseover", function (event, d) {
+
+    // sets innerhtml for headers and text on the right
+    set_html_text(d,'activity');
+
+    // show lists of ACTIVITIES in bottom left
+    activity_bullets(d);
+
+    // show lists of EXPERIENCES in bottom left
+    experience_bullets(d);
+
+  })
+  .on("mouseout", function (event, d) {
+
+    clear_html_text();
+    clear_bullets();
+
+  });
+
 
 // ============================================ //
 // =========== ACTIVITY CIRCLES END =========== //
@@ -309,11 +247,14 @@ svg
     .attr("transform", d => `translate(${xScale(d)}, ${Y_ACTIVITIES-2})`)
     .on("mouseover", function(d) {exp_mouseover(this, d);})
     .on("mouseout", function(d) {exp_mouseout(this, d);});
+
+    
   
   function exp_mouseover(that,d){
     console.log("exp_mouseover", that, d);
     console.log("exp_mouseover", "experiences_circle-"+d);
     console.log("meta",CIRCLE_RADIUS * SIZE_MULTIPLIER);
+    
     // d3.select("#experiences_circle-"+d)
     d3.select(that).selectAll(".experience_circle")
 
@@ -403,14 +344,15 @@ svg
   //   .attr("width", ICON_WIDTH)
   //   .attr("height", ICON_HEIGHT);
   
-// ================================================ //
-// =========== EXPERIENCE CIRCLES (TOP) =========== //
-// ================================================ //
 
 // .attr("id", (d) => `exp-${d}`)
   // .attr("id", (d) => `exp-${d}`)
 
-// Draw circles for unique children
+// ================================================ //
+// =========== EXPERIENCE CIRCLES (TOP) =========== //
+// ================================================ //
+
+// Draw circles for unique children.child-group
 svg
   .append("g")
   .selectAll("circle.children")
@@ -444,8 +386,8 @@ svg
 // Draw text labels for unique children
 svg
   .append("g")
-.attr("id", (d) => `experiences_txt-${d}`)
-.selectAll("text.children")
+  .attr("id", (d) => `experiences_txt-${d}`)
+  .selectAll("text.children")
   .data(uniqueChildren)
   .enter()
   .append("text")
@@ -462,182 +404,39 @@ svg
 d3.selectAll(".experience_circle")
   .on("mouseover", function (event, d) {
 
-        // exp_mouseover(this, d);
-    // SHOW PARENT/ROOT CIRCLES
+    // exp_mouseover(this, d);
 
-    document.getElementById("pux_header_id").innerHTML=pux_list_definitions[d.slice(0, 2)]+" "+ PUX_COMPLETE[d].id[2];
-    document.getElementById("pux_header_id").style.color = colorMap[d.slice(0, 2)];
-    document.getElementById("pux_header_name").innerHTML=": "+PUX_COMPLETE[d].name;
+    clear_bullets();
 
-    document.getElementById("short_paragraph").innerHTML=PUX_COMPLETE[d].short_description;
-    document.getElementById("long_paragraph").innerHTML=PUX_COMPLETE[d].long_description;
+    set_html_text(d, 'experience');
 
+    //bottom left bullets
+    combined_bullets(d, find_experience_parents(d));
 
-    let parent_list = findNameByImport(d);
-    let parent_text = "";
-
-    parent_list.forEach((entry) => {
-      const found_parent = inputData.find((item) => item.name === entry);
-      parent_text += `(${entry})  ${found_parent.id} \n`;
-      d3.select(`#${entry}-${d}`)
-        .style("stroke", colorMap[entry.slice(0, 2)])
-        .style("stroke-width", STROKE_WIDTH_ON)
-        .style("stroke-opacity", OPACITY_ON);
-    });
-
-    // d3.select("#experience_definition").text("");
-    // d3.select("#experience_definition").text(pux_list_definitions[d.slice(0, 2)]);
-
-    d3.selectAll(".temp-circle").remove();
-
-    let split_text = parent_text.split("\n");
-
-    appendCircles(svg, split_text, 497, colorMap);
-
-    d3.select("#activity_txt").text("");
-
-    const foundObject = newData.find((item) => item.name === d);
-    const newExperience = `(${d})  ${foundObject.id} \n`;
-    const splitTextExp = newExperience.split("\n");
-
-    updateText("#activity_txt", split_text,125);
-    updateText("#experience_txt", splitTextExp,125);
-
-    d3.select("#experience_txt")
-      .text("")
-      .text(" (" + d + ")  " + foundObject.id + " \n");
-
-    appendCircles(svg, splitTextExp, 547, colorMap);
-
-    //------------
-
-
+    //highlight relevant icons
+    d3.selectAll(".experience_circle").style("opacity", OPACITY_OFF);
     d3.select(this).style("opacity", OPACITY_ON);
 
-    d3.selectAll(".experiences_path")
-      .style("stroke", STROKE_COLOR_OFF)
-      .style("opacity", OPACITY_OFF)
-      .style("stroke-width", STROKE_WIDTH_OFF);
+    //set all experience paths gray
+    clean_experience_paths();
 
-    // Initialize two arrays to store IDs
-    const firstHalfIDs = [];
-    const secondHalfIDs = [];
-
-    // Select all elements with the class "experiences_path"
-    d3.selectAll(".experiences_path").each(function () {
-      const idParts = this.id.split("-");
-      if (idParts[0].includes(d)) {
-        firstHalfIDs.push(this.id);
-      } else if (idParts[1].includes(d)) {
-        secondHalfIDs.push(this.id);
-      }
-    });
-
-    // Calculate the total length of the path
-    let pathLength;
-
-    let positive_experience = "";
-    let negative_experience = "";
-
-    // Loop through and target elements with specificString in the first half
-    firstHalfIDs.forEach((id) => {
-      const target_circle = id.split("-")[1];
-      const element = d3.select("#" + id);
-      const pathLength = element.node().getTotalLength();
-
-      d3.select("#experiences_circle-" + target_circle)
-        .style("opacity",OPACITY_ON);
-
-      element
-        .attr("stroke-dasharray", `${pathLength} ${pathLength}`)
-        .attr("stroke-dashoffset", pathLength)
-        .style("stroke", FORWARD_LINK_COLOR)
-        .style("stroke-width", STROKE_WIDTH_ON)
-        .style("opacity", OPACITY_ON)
-        .transition()
-        .duration(1500)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0);
-
-      let foundObject =
-        newData.find((item) => item.name === target_circle) ||
-        inputData.find((item) => item.name === target_circle);
-      if (!foundObject)
-        console.error(`id: ${id}, target: ${target_circle} not found`);
-
-      positive_experience += ` (${target_circle})   ${foundObject.id} \n`;
-    });
-
-    // Loop through and target elements with specificString in the second half
-    secondHalfIDs.forEach((id) => {
-      const target_circle = id.split("-")[0];
-      const element = d3.select("#" + id);
-      const pathLength = element.node().getTotalLength();
-
-      let foundObject =
-        newData.find((item) => item.name === target_circle) ||
-        inputData.find((item) => item.name === target_circle);
-      if (!foundObject)
-        console.error(`id: ${id}, target: ${target_circle} not found`);
-
-      negative_experience += ` (${target_circle}) \t ${foundObject.id} \n`;
-
-      d3.select("#experiences_circle-" + target_circle)
-        .style("opacity",OPACITY_ON);
-
-      element
-        .attr("stroke-dasharray", `${pathLength} ${pathLength}`)
-        .attr("stroke-dashoffset", pathLength)
-        .style("stroke", BACKWARD_LINK_COLOR)
-        .style("stroke-width", STROKE_WIDTH_ON)
-        .style("opacity", OPACITY_ON)
-        .transition()
-        .duration(1500)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0);
-    });
-
-    console.log(parent_text);
-
-    let split_positive = positive_experience.split("\n");
-    let split_negative = negative_experience.split("\n");
-
-    d3.select("#negative_experience").text("");
-    d3.select("#positive_experience").text("");
-
-
-    const START_Y = 511.5; // Define accordingly
-
-    updateTextAndCircles("negative_experience",split_negative,START_NEGATIVE_X,START_Y);
-    updateTextAndCircles("positive_experience",split_positive,START_POSITIVE_X, START_Y);
+    // bullets for positive and negative experience correlations
+    experience_sentiments_bullets(d);
   })
   .on("mouseout", function (event, d) {
 
     // exp_mouseout(this, d);
 
-    document.getElementById("pux_header_id").innerHTML="";
-    document.getElementById("pux_header_name").innerHTML="";
+    clear_bullets();
 
-    // d3.select("#experience_definition").text("Hover over");
+    clear_html_text();
+  
+    //targets experience paths and circles
+    clean_experience_paths();
 
-    d3.selectAll(".temp-circle").remove();
+    //targets activity paths only (optional)
+    clean_activities_paths();
 
-    d3.selectAll(".activities_path")
-      .style("stroke", STROKE_COLOR_OFF)
-      .style("stroke-width", STROKE_WIDTH_OFF)
-      .style("stroke-opacity", OPACITY_OFF);
-
-    d3.selectAll(".experiences_path")
-      .style("stroke", STROKE_COLOR_OFF)
-      .style("stroke-width", STROKE_WIDTH_OFF)
-      .style("stroke-opacity", OPACITY_OFF);
-
-    d3.selectAll(".experience_circle")
-      .style("stroke-opacity", OPACITY_ON)
-      .style("opacity", OPACITY_ON);
-
-    d3.select("#positive_experience").text("");
-    d3.select("#negative_experience").text("");
   });
 
 // ============================================== //
@@ -649,15 +448,18 @@ d3.selectAll(".experience_circle")
     // {text: "Hover over an experience", x: 910, y: 250, style: {"font-style": "italic"}, id: "experience_definition"},
     {text: "Activity:", x: 50, y: 500, style: {"font-weight": "bolder"}},
     {text: "Hover", x: 125, y: 500, id: "activity_txt"},
+
     {text: "Experience:", x: 50, y: 550, style: {"font-weight": "bolder"}},
     {text: "Hover", x: 125, y: 550, id: "experience_txt"},
+
     {text: "Positively corelated:", x: START_POSITIVE_X, y: 500, style: {"font-weight": "bolder"}},
     {text: "Select Experience", x: START_POSITIVE_X, y: 515, id: "positive_experience"},
     {text: "Negatively corelated:", x: START_NEGATIVE_X, y: 500, style: {"font-weight": "bolder"}},
     {text: "Select Experience", x: START_NEGATIVE_X, y: 515, id: "negative_experience"},
+
     {text: "Interpretation activities", x: 135, y: 440, style: {"font-style": "italic"}},
-    {text: "Construction activities", x: 450, y: 440, style: {"font-style": "italic"}},
-    {text: "Social activities", x: 750, y: 440, style: {"font-style": "italic"}}
+    {text: "Construction activities", x: 530, y: 440, style: {"font-style": "italic"}},
+    {text: "Social activities", x: 920, y: 440, style: {"font-style": "italic"}}
   ];
   
   
